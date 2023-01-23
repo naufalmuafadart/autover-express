f000ceb74387eff9477f949718733d2471e26f78
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2023-01-23 18:01:17 +0700

    Correct password schema

--- app/infrastructures/validator/auth/schema.js
+++ app/infrastructures/validator/auth/schema.js
@@ -4,12 +4,12 @@ const signUpPayloadSchema = Joi.object({
   full_name : Joi.string().min(1).required(),
   phone_number: Joi.string().min(9).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().alphanum().min(8).required(),
+  password: Joi.string().min(8).required(),
 });
 
 const signInPayloadSchema = Joi.object({
   email: Joi.string().email().required(),
-  password: Joi.string().alphanum().min(8).required(),
+  password: Joi.string().min(8).required(),
 });
 
 module.exports = { signUpPayloadSchema, signInPayloadSchema };
