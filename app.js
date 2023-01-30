7d70b7a8552ae19e3a4f1fe94d29d9a76d78ff80
minidart <opangay@gmail.com>
2023-01-30 09:09:16 +0700

    Use env to store mongo url

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors(corsOptions));
 
 // mongoose
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/autover');
+  await mongoose.connect(process.env.MONGO_URL);
 }
 main().catch((err) => {
   // eslint-disable-next-line no-console
