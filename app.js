1702e6c86813b900e242e134a972eef08619efe3
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2023-01-31 11:29:47 +0700

    install body parser

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const { connect: connectToDB } = require('./config/db');
 
 const adminRouter = require('./app/interfaces/admin/router');
@@ -29,8 +30,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
